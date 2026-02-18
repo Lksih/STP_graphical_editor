df864d3fe54b0a6ee7b931c099d9684c2ff6cd5c
Lksih <odindwatri123123@gmail.com>
2026-02-18 23:55:21 +0700

    IFigure

---
+++ Geometry/IFigure.cs
@@ -0,0 +1,30 @@
+﻿using System.Drawing;
+
+namespace Geometry
+{
+    public record Point(double X, double Y);
+
+    public interface IFigureGraphicProperties
+    {
+        Color Color { get; }
+        double Thickness { get; }
+    }
+
+    public interface IDrawFigure
+    {
+
+    }
+
+    public interface IFigure
+    {
+        Point Center { get; }
+        ReadOnlySpan<Point> Vertex { get; }
+        void Scale(double dx, double dy); // можно отрицательные для отражения
+        void RadialScale(double dr);
+        void Rotate(double angle);
+        void Move(double dx, double dy);
+        void UpdateVertex(ReadOnlySpan<Point> Vertex);
+        IEnumerable<IDrawFigure> Draw();
+        bool IsIn(Point p, double eps);
+    }
+}
