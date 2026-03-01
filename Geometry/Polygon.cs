562f72ad84d5bc3a413edb2f660e80d533daf193
Vladislav200 <robotics.vlad@yandex.ru>
2026-03-01 17:52:19 +0700

    Модификация полигона

--- Geometry/Polygon.cs
+++ Geometry/Polygon.cs
@@ -1,94 +1,68 @@
 using System.Numerics;
+using System.Runtime.InteropServices;
 
 namespace Geometry
 {
     class Polygon : IFigure
     {
-        public Point Center { get; }
-        public List<Point> Vertex { get; }
+        public Point Center { get; private set;}
+        public ReadOnlySpan<Point> Vertex { get => CollectionsMarshal.AsSpan(VertList); }
+        private List<Point> VertList;
 
-        public Polygon()
+        public Polygon(ReadOnlySpan<Point> Verts)
         {
-            Center = new Point();
-            Vertex = new List<Point>{};
-        }
-        public void AddPoint(double x, double y)
-        {
-            Point point = new Point();
-            point.X = x;
-            point.Y = y;
-            Vertex.Add(point);
-            double sumx = 0, sumy = 0;
-            foreach (var vert in Vertex)
-            {
-                sumx += vert.X;
-                sumy += vert.Y;
-            }
-            //Дописать алгоритм поиска центроида(Вариант - алгоритм Вайсфельда)
+            Center = new Point(0, 0);
+            VertList = [.. Verts];
+            foreach (var vert in VertList)
+                Center.Addition(vert);
+            Center.Multiply(1 / VertList.Count());
         }
         public void Scale(double dx, double dy)
         {
-            foreach (var vert in Vertex)
+            for (int i = 0; i < VertList.Count(); i++)
             {
-                vert.X = Center.X + (vert.X - Center.X) * dx;
-                vert.Y = Center.Y + (vert.Y - Center.Y) * dy;
+                Point dist = VertList[i] - Center;
+                dist.X *= dx;
+                dist.Y *= dy;
+                VertList[i] = Center + dist;
             }
         }
         public void RadialScale(double dr)
         {
-            foreach (var vert in Vertex)
+            for (int i = 0; i < VertList.Count(); i++)
             {
-                vert.X = Center.X + (vert.X - Center.X) * dr;
-                vert.Y = Center.Y + (vert.Y - Center.Y) * dr;
+                VertList[i] = Center + (VertList[i] - Center) * dr;
             }
         }
         public void Rotate(double angle)
         {
-            foreach (var vert in Vertex)
+            for (int i = 0; i < VertList.Count(); i++)
             {
+                Point vert = VertList[i];
                 double tx = vert.X - Center.X, ty = vert.Y - Center.Y, currAngle = Math.Atan2(ty, tx), distance = Math.Sqrt(Math.Pow(tx, 2) + Math.Pow(ty, 2));
-                vert.X = Center.X + distance * Math.Cos(angle + currAngle);
-                vert.Y = Center.Y + distance * Math.Sin(angle + currAngle);
+                Point d = new Point(Math.Cos(angle + currAngle), Math.Sin(angle + currAngle));
+                d.Multiply(distance);
+                VertList[i] = Center + d;
             }
         }
         public void Move(double dx, double dy)
         {
-            foreach (var vert in Vertex)
-            {
-                vert.X += dx;
-                vert.Y += dy;
-            }
-            Center.X += dx;
-            Center.Y += dy;
+            Point d = new Point(dx, dy);
+            for (int i = 0; i < VertList.Count(); i++)
+                VertList[i].Addition(d);
+            Center.Addition(d);
         }
-        public void UpdateVertex(int index, double x, double y)
+        public void UpdateVertex(ReadOnlySpan<Point> NewVertex)
         {
-            Vertex[index].X = x;
-            Vertex[index].Y = y;
-            Center.X = Math.Min(Vertex[0].X, Vertex[1].X) + (Math.Max(Vertex[0].X, Vertex[1].X) - Math.Min(Vertex[0].X, Vertex[1].X)) / 2.0; 
-            Center.Y = Math.Min(Vertex[0].Y, Vertex[1].Y) + (Math.Max(Vertex[0].Y, Vertex[1].Y) - Math.Min(Vertex[0].Y, Vertex[1].Y)) / 2.0;
-            //Также дописать алгоритм поиска центроида
+            VertList = [.. NewVertex];
+            foreach (var vert in VertList)
+                Center.Addition(vert);
+            Center.Multiply(1 / VertList.Count());
         }
-        public IEnumerable<IDrawFigure> Draw()
-        {
-            
-        }
+        public IEnumerable<IDrawFigure> Draw() => throw new NullReferenceException();
         public bool IsIn(Point p, double eps)
         {
-            if (Vertex[0].X == Vertex[1].X)
-            {
-                return Math.Abs(p.X - Vertex[0].X) <= eps && Math.Min(Vertex[0].Y, Vertex[1].Y) - eps <= p.Y && p.Y <= Math.Max(Vertex[0].Y, Vertex[1].Y) - eps;
-            }
-            else if (Vertex[0].Y == Vertex[1].Y)
-            {
-                return Math.Abs(p.Y - Vertex[0].Y) <= eps && Math.Min(Vertex[0].X, Vertex[1].X) - eps <= p.X && p.X <= Math.Max(Vertex[0].X, Vertex[1].X) - eps;
-            }
-            else
-            {
-                double t1 = (p.X - Vertex[0].X) / (Vertex[1].X - Vertex[0].X), t2 = (p.Y - Vertex[0].Y) / (Vertex[1].Y - Vertex[0].Y), lenght = Math.Sqrt(Math.Pow(Vertex[0].X - Vertex[1].X, 2) + Math.Pow(Vertex[0].Y - Vertex[1].Y, 2));
-                return Math.Abs(t1 - t2) * lenght <= 2 * eps && (Math.Max(t2, t1) - 1) * lenght <= eps && Math.Min(t2, t1) * lenght >= -eps;
-            }
-            //Реализовать алгоритм попадания точки с использованием псевдонормалей
+            throw new NullReferenceException();
         }
     }
 }
